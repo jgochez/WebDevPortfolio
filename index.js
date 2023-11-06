1e2453acfa261bcfe9d48d45f7302ea0b8586d19
jovanny-macbookair <jovannygochez23@gmail.com>
2023-11-06 14:53:19 -0600

    a6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const productsFile = require('./products.js').products;
+const productsFile = require('/products.js').products;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
