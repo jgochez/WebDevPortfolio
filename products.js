7e1f67c01fb571525beddd995856065a6b6e2194
jovanny-macbookair <jovannygochez23@gmail.com>
2023-11-06 14:47:09 -0600

    a6

--- products.js
+++ products.js
@@ -1,57 +1,5 @@
 'use strict';
 
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-let htmlTop = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
-    <title>Jovanny Gochez</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png"> 
-    <link rel="icon" type="image/png" sizes="512x512" href="android-chrome-512x512.png"> 
-    <link rel="icon" type="image/png" sizes="192x192" href="android-chrome-192x192.png"> 
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png"> 
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png"> 
-    <link rel="manifest" href="site.webmanifest">
-    
-</head>
-<body>
-    <header>
-        <img src="android-chrome-192x192.png" id="hero-image">
-        <h1 id="hero-name">
-            Jovanny Gochez
-        </h1>
-    </header>
-    <nav class="global-nav">
-        <a href="index.html">Index</a>
-        <a href="contact.html">Contact</a>
-        <a href="gallery.html">Gallery</a>
-        <a href="order.html">Order</a>
-    </nav>
-    <main>
-`;
-
-let htmlBottom= `
-</main>
-    <footer>
-        <p>
-            &copy; 2023 Jovanny Gochez
-        </p>
-    </footer>
-</body>
-</html>
-`;
-
-
 // Define a JavaScript object with arrays of products, their company names, and prices: 
 const products = [
     { company: 'AGYM', product: 'Spinning Cat Scratcher Ball', price: 52.99 },
@@ -69,16 +17,4 @@ module.exports.products = products;
 // https://developer.mozilla.org/en-US/docs/Web/API/Console/table
 console.table(products); 
 
-app.post("/submit-form-data", (req, res) => {
-    
-res.send(`
-    ${htmlTop}
-    <section>
-        <h2>Response</h2>
-        <article>
-            <p>Currently under production!</p>
-        </article>
-    </section>
-    ${htmlBottom}`)});
  
-app.listen(PORT, () => { console.log(`Server listening on port ${PORT}...`); });
